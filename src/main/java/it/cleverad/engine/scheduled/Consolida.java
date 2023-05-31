d03749001b8eebadbf07fda4b72e7a6de3b4f02e
lah <ivo.lachi@dotcom.ts.it>
2023-05-31 21:00:18 +0200

    consolidamento cpc e cpm

--- src/main/java/it/cleverad/engine/scheduled/Consolida.java
+++ src/main/java/it/cleverad/engine/scheduled/Consolida.java
@@ -6,6 +6,7 @@ import it.cleverad.engine.business.TransactionBusiness;
 import it.cleverad.engine.web.dto.AffiliateChannelCommissionCampaignDTO;
 import it.cleverad.engine.web.dto.CampaignDTO;
 import it.cleverad.engine.web.dto.TransactionCPCDTO;
+import it.cleverad.engine.web.dto.TransactionCPMDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -29,40 +30,128 @@ public class Consolida {
     @Autowired
     private AffiliateChannelCommissionCampaignBusiness acccBusiness;
 
-    @Scheduled(cron = "30 * * * * ?")
+    @Scheduled(cron = "0 59 * * * ?")
     public void consolidaCPC() {
-
-        log.info(LocalDateTime.now().toString() + "");
-        log.info(LocalDateTime.now().getDayOfYear() + "  " + LocalDateTime.now().getDayOfMonth());
 
         List<CampaignDTO> camps = campaignBusiness.getEnabledCampaigns();
         List<AffiliateChannelCommissionCampaignDTO> acccs = new ArrayList<>();
         camps.stream().forEach(campaignDTO -> {
-            acccs.addAll(acccBusiness.searchByCampaignId(campaignDTO.getId(), PageRequest.of(0, Integer.MAX_VALUE, Sort.by(Sort.Order.asc("id")))).getContent());
+            acccs.addAll(acccBusiness.searchByCampaignIdAndType(campaignDTO.getId(), 10L, PageRequest.of(0, Integer.MAX_VALUE, Sort.by(Sort.Order.asc("id")))).getContent());
         });
-        log.info(acccs.size() + " == ACCC");
         for (AffiliateChannelCommissionCampaignDTO dto : acccs) {
+            log.info("ACCC CPC :: {}  - {}  - {}", dto.getCampaignId(), dto.getAffiliateId(), dto.getChannelId());
 
             TransactionBusiness.Filter request = new TransactionBusiness.Filter();
-//            LocalDateTime oraSpaccata = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
-//            request.setDateTimeFrom(oraSpaccata.minusHours(3));
-//            request.setDateTimeTo(oraSpaccata);
             LocalDateTime oraSpaccata = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
             request.setDateTimeFrom(oraSpaccata.toLocalDate().atStartOfDay());
             request.setDateTimeTo(oraSpaccata);
             request.setCampaignId(dto.getCampaignId());
             request.setAffiliateId(dto.getAffiliateId());
             request.setChannelId(dto.getChannelId());
-            Page<TransactionCPCDTO> cpcs = transactionBusiness.searchCpc(request, PageRequest.of(0, 1000, Sort.by(Sort.Order.asc("id"))));
+            //request.setCommissionId(dto.getCommissionId());
+            Page<TransactionCPCDTO> cpcs = transactionBusiness.searchCpc(request, PageRequest.of(0, Integer.MAX_VALUE, Sort.by(Sort.Order.asc("id"))));
 
-            cpcs.stream().forEach(tcpc -> {
-                log.info("TRANSAZIONE CPC :: {}", tcpc.getId());
-            });
+            Long totaleClick = 0L;
+            Double value = 0D;
+            Long walletId = null;
+            Long mediaId = null;
+            Long dictionaryId = null;
+            Long revenueId = null;
+            for (TransactionCPCDTO tcpc : cpcs) {
+                totaleClick += tcpc.getClickNumber();
+                value += tcpc.getValue();
+                walletId = tcpc.getWalletId();
+                mediaId = tcpc.getMediaId();
+                dictionaryId = tcpc.getDictionaryId();
+                revenueId = tcpc.getRevenueId();
+                log.info("TRANSAZIONE CPC ID :: {} : {} :: {}", tcpc.getId(), tcpc.getClickNumber(), tcpc.getDateTime());
+                transactionBusiness.deleteInterno(tcpc.getId(), "CPC");
+            }
+            if (totaleClick > 0) {
+                log.info("Click {}  x valore {}\n\n", totaleClick, value);
 
 
+                TransactionBusiness.BaseCreateRequest bReq = new TransactionBusiness.BaseCreateRequest();
+                bReq.setClickNumber(totaleClick);
+                bReq.setValue(value);
+                bReq.setCampaignId(dto.getCampaignId());
+                bReq.setAffiliateId(dto.getAffiliateId());
+                bReq.setChannelId(dto.getChannelId());
+                bReq.setCommissionId(dto.getCommissionId());
+                bReq.setApproved(true);
+                bReq.setDateTime(oraSpaccata.minusMinutes(1));
+                bReq.setMediaId(mediaId);
+                bReq.setDictionaryId(dictionaryId);
+                bReq.setRevenueId(revenueId);
+                bReq.setWalletId(walletId);
+                bReq.setAgent("");
+                TransactionCPCDTO cpc = transactionBusiness.createCpc(bReq);
+            }
         }
 
 
     }//trasformaTrackingCPC
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void consolidaCPM()  {
+
+        List<CampaignDTO> camps = campaignBusiness.getEnabledCampaigns();
+        List<AffiliateChannelCommissionCampaignDTO> acccs = new ArrayList<>();
+        camps.stream().forEach(campaignDTO -> {
+            acccs.addAll(acccBusiness.searchByCampaignIdAndType(campaignDTO.getId(), 50L, PageRequest.of(0, Integer.MAX_VALUE, Sort.by(Sort.Order.asc("id")))).getContent());
+        });
+        for (AffiliateChannelCommissionCampaignDTO dto : acccs) {
+            log.info("ACCC CPM :: {}  - {}  - {}", dto.getCampaignId(), dto.getAffiliateId(), dto.getChannelId());
+
+            TransactionBusiness.Filter request = new TransactionBusiness.Filter();
+            LocalDateTime oraSpaccata = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
+            request.setDateTimeFrom(oraSpaccata.toLocalDate().atStartOfDay());
+            request.setDateTimeTo(oraSpaccata);
+            request.setCampaignId(dto.getCampaignId());
+            request.setAffiliateId(dto.getAffiliateId());
+            request.setChannelId(dto.getChannelId());
+            //request.setCommissionId(dto.getCommissionId());
+            Page<TransactionCPMDTO> cpcs = transactionBusiness.searchCpm(request, PageRequest.of(0, Integer.MAX_VALUE, Sort.by(Sort.Order.asc("id"))));
+
+            Long impressionNumber = 0L;
+            Double value = 0D;
+            Long walletId = null;
+            Long mediaId = null;
+            Long dictionaryId = null;
+            Long revenueId = null;
+            for (TransactionCPMDTO tcpm : cpcs) {
+                impressionNumber += tcpm.getImpressionNumber();
+                value += tcpm.getValue();
+                walletId = tcpm.getWalletId();
+                mediaId = tcpm.getMediaId();
+                dictionaryId = tcpm.getDictionaryId();
+                revenueId = tcpm.getRevenueId();
+                log.info("TRANSAZIONE CPM ID :: {} : {} :: {}", tcpm.getId(), tcpm.getImpressionNumber(), tcpm.getDateTime());
+                //transactionBusiness.deleteInterno(tcpc.getId(), "CPM");
+            }
+            if (impressionNumber > 0) {
+                log.info("impression {}  x valore {}\n\n", impressionNumber, value);
+
+                TransactionBusiness.BaseCreateRequest bReq = new TransactionBusiness.BaseCreateRequest();
+                bReq.setImpressionNumber(impressionNumber);
+                bReq.setValue(value);
+                bReq.setCampaignId(dto.getCampaignId());
+                bReq.setAffiliateId(dto.getAffiliateId());
+                bReq.setChannelId(dto.getChannelId());
+                bReq.setCommissionId(dto.getCommissionId());
+                bReq.setApproved(true);
+                bReq.setDateTime(oraSpaccata.minusMinutes(1));
+                bReq.setMediaId(mediaId);
+                bReq.setDictionaryId(dictionaryId);
+                bReq.setRevenueId(revenueId);
+                bReq.setWalletId(walletId);
+                bReq.setAgent("");
+                //transactionBusiness.createCpm(bReq);
+            }
+        }
+
+
+    }//trasformaTrackingCPC
+
 
 }
