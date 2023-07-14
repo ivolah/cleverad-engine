10f306907d368a0a28859cd362407ab5d6355aff
lah <ivo.lachi@dotcom.ts.it>
2023-07-14 12:34:28 +0200

    gestione channel ALL su asseganzione

--- src/main/java/it/cleverad/engine/business/AffiliateChannelCommissionCampaignBusiness.java
+++ src/main/java/it/cleverad/engine/business/AffiliateChannelCommissionCampaignBusiness.java
@@ -1,7 +1,10 @@
 package it.cleverad.engine.business;
 
 import com.github.dozermapper.core.Mapper;
-import it.cleverad.engine.persistence.model.service.*;
+import it.cleverad.engine.persistence.model.service.Affiliate;
+import it.cleverad.engine.persistence.model.service.AffiliateChannelCommissionCampaign;
+import it.cleverad.engine.persistence.model.service.Campaign;
+import it.cleverad.engine.persistence.model.service.Commission;
 import it.cleverad.engine.persistence.repository.service.*;
 import it.cleverad.engine.service.JwtUserDetailsService;
 import it.cleverad.engine.service.MailService;
@@ -11,6 +14,7 @@ import it.cleverad.engine.web.exception.PostgresDeleteCleveradException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,7 @@ import javax.persistence.criteria.Predicate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -43,6 +48,8 @@ public class AffiliateChannelCommissionCampaignBusiness {
     @Autowired
     private ChannelRepository channelRepository;
     @Autowired
+    private ChannelBusiness channelBusiness;
+    @Autowired
     private CommissionRepository commissionRepository;
     @Autowired
     private CampaignAffiliateBusiness campaignAffiliateBusiness;
@@ -57,32 +64,49 @@ public class AffiliateChannelCommissionCampaignBusiness {
 
     // CREATE
     public AffiliateChannelCommissionCampaignDTO create(BaseCreateRequest request) {
+        if (request.getChannelId() == 0) {
+            log.info(request.toString());
+            channelBusiness.getbyIdAffiliateAllActive(request.getAffiliateId()).forEach(idCanale -> {
+                AffiliateChannelCommissionCampaign map = mapper.map(request, AffiliateChannelCommissionCampaign.class);
+                map.setCreationDate(LocalDateTime.now());
+                map.setLastModificationDate(LocalDateTime.now());
 
-        AffiliateChannelCommissionCampaign map = mapper.map(request, AffiliateChannelCommissionCampaign.class);
-        map.setCreationDate(LocalDateTime.now());
-        map.setLastModificationDate(LocalDateTime.now());
+                Affiliate affiliate = affiliateRepository.findById(request.getAffiliateId()).orElseThrow(() -> new ElementCleveradException("Affilirte", request.getAffiliateId()));
+                map.setAffiliate(affiliate);
+                Commission commission = commissionRepository.findById(request.getCommissionId()).orElseThrow(() -> new ElementCleveradException("Commission", request.getCommissionId()));
+                map.setCommission(commission);
+                Campaign campaign = campaignRepository.findById(request.getCampaignId()).orElseThrow(() -> new ElementCleveradException("Campaign", request.getCampaignId()));
+                map.setCampaign(campaign);
+                map.setChannel(channelRepository.findById(idCanale).orElseThrow(() -> new ElementCleveradException("Channel", idCanale)));
+                AffiliateChannelCommissionCampaign dto = repository.save(map);
+            });
 
-        Affiliate affiliate = affiliateRepository.findById(request.getAffiliateId()).orElseThrow(() -> new ElementCleveradException("Affilirte", request.getAffiliateId()));
-        map.setAffiliate(affiliate);
-        Channel channel = channelRepository.findById(request.getChannelId()).orElseThrow(() -> new ElementCleveradException("Channel", request.getChannelId()));
-        map.setChannel(channel);
-        Commission commission = commissionRepository.findById(request.getCommissionId()).orElseThrow(() -> new ElementCleveradException("Commission", request.getCommissionId()));
-        map.setCommission(commission);
-        Campaign campaign = campaignRepository.findById(request.getCampaignId()).orElseThrow(() -> new ElementCleveradException("Campaign", request.getCampaignId()));
-        map.setCampaign(campaign);
+        } else {
+            AffiliateChannelCommissionCampaign map = mapper.map(request, AffiliateChannelCommissionCampaign.class);
+            map.setCreationDate(LocalDateTime.now());
+            map.setLastModificationDate(LocalDateTime.now());
 
-        if (campaignAffiliateBusiness.searchByAffiliateIdAndCampaignId(affiliate.getId(), campaign.getId()).getTotalElements() == 0) {
-            // assicio affiliate a campagna
-            campaignAffiliateBusiness.create(new CampaignAffiliateBusiness.BaseCreateRequest(campaign.getId(), affiliate.getId(), null, null));
-            // inoltro mail di invito a  camapgna
-            MailService.BaseCreateRequest reqMail = new MailService.BaseCreateRequest();
-            reqMail.setAffiliateId(affiliate.getId());
-            reqMail.setCampaignId(campaign.getId());
-            mailService.invitoCampagna(reqMail);
+            Affiliate affiliate = affiliateRepository.findById(request.getAffiliateId()).orElseThrow(() -> new ElementCleveradException("Affilirte", request.getAffiliateId()));
+            map.setAffiliate(affiliate);
+            Commission commission = commissionRepository.findById(request.getCommissionId()).orElseThrow(() -> new ElementCleveradException("Commission", request.getCommissionId()));
+            map.setCommission(commission);
+            Campaign campaign = campaignRepository.findById(request.getCampaignId()).orElseThrow(() -> new ElementCleveradException("Campaign", request.getCampaignId()));
+            map.setCampaign(campaign);
+            //canale Singolo
+            map.setChannel(channelRepository.findById(request.getChannelId()).orElseThrow(() -> new ElementCleveradException("Channel", request.getChannelId())));
+            AffiliateChannelCommissionCampaignDTO.from(repository.save(map));
         }
 
-
-        return AffiliateChannelCommissionCampaignDTO.from(repository.save(map));
+        if (campaignAffiliateBusiness.searchByAffiliateIdAndCampaignId(request.getAffiliateId(), request.getCampaignId()).getTotalElements() == 0) {
+            // assicio affiliate a campagna
+            campaignAffiliateBusiness.create(new CampaignAffiliateBusiness.BaseCreateRequest(request.getCampaignId(), request.getAffiliateId(), null, null));
+            // inoltro mail di invito a  camapgna
+            MailService.BaseCreateRequest reqMail = new MailService.BaseCreateRequest();
+            reqMail.setAffiliateId(request.getAffiliateId());
+            reqMail.setCampaignId(request.getCampaignId());
+            mailService.invitoCampagna(reqMail);
+        }
+        return null;
     }
 
     // DELETE BY ID
@@ -221,6 +245,7 @@ public class AffiliateChannelCommissionCampaignBusiness {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @ToString
     public static class BaseCreateRequest {
         private Long campaignId;
         private Long affiliateId;
