b368af7bb8b6d088b5bd85fdf82bf69165c1ee67
lah <ivo.lachi@dotcom.ts.it>
2023-11-03 13:56:09 +0100

    commentato Telegram

--- src/main/java/it/cleverad/engine/service/telegram/app/BotConfigImpl.java
+++ src/main/java/it/cleverad/engine/service/telegram/app/BotConfigImpl.java
@@ -1,28 +1,28 @@
-package it.cleverad.engine.service.telegram.app;
-
-import org.telegram.bot.structure.BotConfig;
-
-public class BotConfigImpl extends BotConfig {
-
-    private String phoneNumber;
-
-    public BotConfigImpl(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-        setAuthfile(phoneNumber + ".auth");
-    }
-
-    @Override
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Override
-    public String getBotToken() {
-        return null;
-    }
-
-    @Override
-    public boolean isBot() {
-        return false;
-    }
-}
+//package it.cleverad.engine.service.telegram.app;
+//
+//import org.telegram.bot.structure.BotConfig;
+//
+//public class BotConfigImpl extends BotConfig {
+//
+//    private String phoneNumber;
+//
+//    public BotConfigImpl(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//        setAuthfile(phoneNumber + ".auth");
+//    }
+//
+//    @Override
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isBot() {
+//        return false;
+//    }
+//}
