226c47e0bf6b186e1ccf5dd7f5b4a1b1242e6905
lah <ivo.lachi@dotcom.ts.it>
2023-11-30 17:10:17 +0100

    Tolto Telegram e Whatsapp

--- src/main/java/it/cleverad/engine/scheduled/CheckTelegram.java
+++
@@ -1,9 +0,0 @@
-package it.cleverad.engine.scheduled;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class CheckTelegram {
-
-
-}
--- src/main/java/it/cleverad/engine/service/telegram/app/TelegramLoginException.java
+++
@@ -1,7 +0,0 @@
-package it.cleverad.engine.service.telegram.app;
-
-public class TelegramLoginException extends Exception {
-    public TelegramLoginException(String message) {
-        super(message);
-    }
-}
