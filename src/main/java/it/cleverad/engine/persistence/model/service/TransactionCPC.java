e94f28c215f0b6149ec20d33382ec90013f4bd42
lah <ivo.lachi@dotcom.ts.it>
2023-12-18 11:33:56 +0100

    Modifica api e riorganizzazinoe

--- src/main/java/it/cleverad/engine/persistence/model/service/TransactionCPC.java
+++ src/main/java/it/cleverad/engine/persistence/model/service/TransactionCPC.java
@@ -25,7 +25,6 @@ public class TransactionCPC {
     private Double initialValue;
     private Double value;
     private Boolean approved;
-
     private String ip;
     private String agent;
     @Column(name = "click_number")
@@ -39,42 +38,32 @@ public class TransactionCPC {
     private LocalDateTime creationDate = LocalDateTime.now();
     @Column(name = "last_modification_date")
     private LocalDateTime lastModificationDate = LocalDateTime.now();
-
     @ManyToOne
     @JoinColumn(name = "wallet_id")
     private Wallet wallet;
-
     @ManyToOne
     @JoinColumn(name = "affiliate_id")
     private Affiliate affiliate;
-
     @ManyToOne
     @JoinColumn(name = "campaign_id")
     private Campaign campaign;
-
     @ManyToOne
     @JoinColumn(name = "commission_id")
     private Commission commission;
-
     @ManyToOne()
     @JoinColumn(name = "channel_id")
     private Channel channel;
-
     @ManyToOne
     @JoinColumn(name = "media_id")
     private Media media;
-
     @ManyToOne
     @JoinColumn(name = "payout_id")
     private Payout payout;
-
     @ManyToOne
     @JoinColumn(name = "dictionary_id")
     private Dictionary dictionary;
-
     @Column(name = "revenue_id")
     private Long revenueId;
-
     @ManyToOne
     @JoinColumn(name = "status_id")
     private Dictionary dictionaryStatus;
