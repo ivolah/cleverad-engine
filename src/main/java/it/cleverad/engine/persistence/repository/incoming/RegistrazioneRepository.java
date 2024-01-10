f1df497e9e161f1b09ea23a7b67b32fd7a5a27aa
lah <ivo@lachilah.it>
2024-01-10 11:56:07 +0100

    Tolto riferimenti incoming

--- src/main/java/it/cleverad/engine/persistence/repository/incoming/RegistrazioneRepository.java
+++
@@ -1,8 +0,0 @@
-//package it.cleverad.engine.persistence.repository.incoming;
-//
-//import it.cleverad.engine.persistence.model.incoming.Registrazione;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-//
-//public interface RegistrazioneRepository extends JpaRepository<Registrazione, Long>, JpaSpecificationExecutor<Registrazione> {
-//}
